8a1503868b85a1d843a8bd0501683699463017b1
Prusch, Maximilian <maximilian.prusch@outlook.com>
2019-01-08 00:22:25 +0100

    Fix static content for express app

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static('dist'));
+app.use(express.static('client/build'));
 
 app.use('/', indexRouter);
 
