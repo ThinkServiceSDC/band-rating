ff2d584ae2cdb0589f0201936ae791eac5778441
Prusch, Maximilian <maximilian.prusch@outlook.com>
2019-01-19 15:58:55 +0100

    replace simulate with prop call for frontend test

--- client/src/Evaluation.test.jsx
+++ client/src/Evaluation.test.jsx
@@ -10,13 +10,13 @@ describe('Evaluation', () => {
 
     it('should change state when selecting different option', () => {
         const wrapper = shallow(<Evaluation/>);
-        wrapper.find('#Good').simulate('change', {target: {value: 'Good'}});
+        wrapper.find('#Good').prop('onChange')({target: {value: 'Good'}});
         expect(wrapper.state()).toEqual({selectedVote: 'Good'});
     });
 
     it('should send data to server', () => {
         const wrapper = shallow(<Evaluation/>);
-        wrapper.find('#Good').simulate('change', {target: {value: 'Good'}});
+        wrapper.find('#Good').prop('onChange')({target: {value: 'Good'}});
         wrapper.find('#Evaluate').simulate('click');
         expect('')
     });
